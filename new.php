516b1f203ee5cd4f29e2fb0d5ee241fc51cd62e9
Jade Allen Cook <jadeallencook@Jades-Macbook-Pro.local>
2018-05-24 12:26:55 -0600

    🔌 Created tool to generate new projects

---
+++ new.php
@@ -0,0 +1,51 @@
+<?php
+
+// get parameters from arguments
+$name = $argv[1];
+
+// create new scss and js files
+$file = fopen("src/styles/" . $name . ".scss", "w") or die("Unable to open file!");
+fwrite($file, "div.interactive[data-type=\"" . $name . "\"] {}");
+fclose($file);
+$file = fopen("src/scripts/" . $name . ".js", "w") or die("Unable to open file!");
+fwrite($file, "export default function " . ucwords($name) . "(container) {}");
+fclose($file);
+
+// app.js
+$string = file_get_contents("src/scripts/app.js");
+$string = str_replace("\nwindow.Newsroom", "import " . ucwords($name) . " from './" . $name . ".js';\n\nwindow.Newsroom", $string);
+$string = str_replace("else console.warn", "else if (type === '" . $name . "') new " . ucwords($name) . "(interactive);\n\t\t\telse console.warn", $string);
+$file = fopen("src/scripts/app.js", "w") or die("Unable to open file!");
+fwrite($file, $string);
+fclose($file);
+
+// main.scss
+$string = file_get_contents("src/styles/main.scss");
+$string = $string . "\n@import \"./" . $name . ".scss\";";
+$file = fopen("src/styles/main.scss", "w") or die("Unable to open file!");
+fwrite($file, $string);
+fclose($file);
+
+// example app.js
+$string = file_get_contents("examples/app.js");
+$string = str_replace("var interactives = {", "var interactives = {\n\t\t" . $name . ": {\n\t\t\ttype: \"" . $name . "\"\n\t\t},", $string);
+$file = fopen("examples/app.js", "w") or die("Unable to open file!");
+fwrite($file, $string);
+fclose($file);
+
+// main app.js
+$string = file_get_contents("app.js");
+$string = str_replace("var interactives = {", "var interactives = {\n\t" . $name . ": '<div class=\"interactive\" data-type=\"" . $name . "\"></div>',", $string);
+$file = fopen("examples/app.js", "w") or die("Unable to open file!");
+fwrite($file, $string);
+fclose($file);
+
+// index.html
+$string = file_get_contents("index.html");
+$interactive = file_get_contents("src/templates/interactive-template.html");
+$interactive = str_replace("{{capital}}", ucwords($name), $interactive);
+$interactive = str_replace("{{lowercase}}", $name, $interactive);
+$string = str_replace("<!-- add new interactives below -->", "<!-- add new interactives below -->\n" . $interactive, $string);
+$file = fopen("index.html", "w") or die("Unable to open file!");
+fwrite($file, $string);
+fclose($file);
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -25,7 +25,7 @@ window.Newsroom = {
             // follow pattern below to add new interactives
             else if (type === 'list') new List(interactive, Newsroom.parameter(interactive, 'src'), Newsroom.parameter(interactive, 'style'));
             else if (type === 'timeline') new Timeline(interactive, Newsroom.parameter(interactive, 'src'), Newsroom.parameter(interactive, 'color'), Newsroom.parameter(interactive, 'title'), Newsroom.parameter(interactive, 'height'));
-            console.warn('Newsroom Interactives: There was no type set for the interactive.');
+			else console.warn('Newsroom Interactives: There was no type set for the interactive.');
         }
     }
 }
