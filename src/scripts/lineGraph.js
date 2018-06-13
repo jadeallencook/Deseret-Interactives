f509a613dfa7fd93a1959c79dbb20fac720c4bc8
Jade Allen Cook <jadeallencook@Jades-Macbook-Pro.local>
2018-06-13 15:53:00 -0600

    📉 Added lines to line chart

--- src/scripts/lineGraph.js
+++ src/scripts/lineGraph.js
@@ -42,7 +42,14 @@ export default function LineGraph(container) {
         R.a(container, graph);
 
         // insert values into graph
+        var lineNum = 0,
+            keys = R.c('div.keys');
         R.e(response.lines, function (line) {
+            var key = R.c('div.key', line.title);
+            var color = R.c('div.color');
+            if (line.color) color.style.backgroundColor = line.color;
+            R.a(key, color);
+            R.a(keys, key);
             var num = 0;
             R.e(line.values, function (value) {
                 var dot = R.c('div.dot');
@@ -51,25 +58,57 @@ export default function LineGraph(container) {
                 var elem = document.getElementById('row-' + row).childNodes[num + 1];
                 dot.style.marginLeft = (elem.offsetWidth - 6) + 'px';
                 dot.style.marginTop = ((elem.offsetHeight * ((row - value) * 0.5)) - 2) + 'px';
+                dot.setAttribute('id', 'line-' + lineNum + '-dot-' + num);
                 if (line.color) dot.style.borderColor = line.color;
+                R.a(elem, dot);
+                if (num > 0) {
+                    var getPositionAtCenter = function (element) {
+                        var data = element.getBoundingClientRect();
+                        return {
+                            x: data.left + data.width / 2,
+                            y: data.top + data.height / 2
+                        };
+                    };
+
+                    var distance = function (a, b) {
+                        var aPosition = getPositionAtCenter(a);
+                        var bPosition = getPositionAtCenter(b);
+
+                        return Math.sqrt(
+                            Math.pow(aPosition.x - bPosition.x, 2) +
+                            Math.pow(aPosition.y - bPosition.y, 2)
+                        ) - 7;
+                    };
+                    var connector = R.c('div.line');
+                    if (line.color) connector.style.borderColor = line.color;
+                    R.a(dot, connector);
+                    var elem1 = 'line-' + lineNum + '-dot-' + (num - 1),
+                        elem2 = 'line-' + lineNum + '-dot-' + num;
+                    elem1 = document.getElementById(elem1);
+                    elem2 = document.getElementById(elem2);
+                    var width = distance(elem1, elem2);
+                    if (width > 40) width = width + 3;
+                    connector.style.width = width + 'px';
+                    var deg = Math.atan2(elem2.getBoundingClientRect().top - elem1.getBoundingClientRect().top, elem2.getBoundingClientRect().left - elem1.getBoundingClientRect().left) * 180 / Math.PI;
+                    if (deg < 0) deg = deg - 3;
+                    else deg = deg + 3;
+                    connector.style.transform = 'rotate(' + deg + 'deg)';
+                }
                 dot.onmouseover = function () {
                     R.a(this, R.c('div.hud', this.getAttribute('data-value')));
                 }
                 dot.onclick = function () {
-                    R.e(document.querySelectorAll('div.dot'), function (clear) {
-                        clear.innerHTML = '';
-                    });
+                    document.querySelector('div.hud').remove();
                     R.a(this, R.c('div.hud', this.getAttribute('data-value')));
                 }
                 dot.onmouseout = function () {
-                    R.e(document.querySelectorAll('div.dot'), function (clear) {
-                        clear.innerHTML = '';
-                    });
+                    document.querySelector('div.hud').remove();
                 }
-                R.a(elem, dot);
                 num++;
-            })
+            });
+            lineNum++;
         });
+        R.a(container, keys);
 
         // footer
         R.a(container, R.c('span', response.footer));
